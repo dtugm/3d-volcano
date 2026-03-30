1c55ae2eb0cc07793fabd56532eb0de3d38be6b6
aldoarya <arudo.saka@gmail.com>
2026-03-30 10:30:12 +0700

    feat: switch to dual terrain viewer when terrain comparison is active

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
+"use client";
+
 import CesiumViewer from "@/components/cesium/CesiumViewerDynamic";
+import DualTerrainViewer from "@/components/cesium/DualTerrainViewerDynamic";
 import SplitSlider from "@/components/cesium/SplitSliderDynamic";
 import LeftSideBar from "@/components/left-sidebar";
+import { useVolcano } from "@/lib/volcano";
 // import RightSidebar from "@/components/right-sidebar";
 
 export default function Home() {
+  const { comparisonEnabled, comparisonMode } = useVolcano();
+
+  const showDualViewer = comparisonEnabled && comparisonMode === "terrain";
+
   return (
     <div className="flex flex-col lg:flex-row h-full w-full overflow-auto">
       <LeftSideBar />
       <main className="relative flex-1 bg-[#F1F5F9] dark:bg-[#0F1419] order-first lg:order-0 min-h-[calc(100vh-64px)] lg:min-h-0">
-        <CesiumViewer />
+        {showDualViewer ? <DualTerrainViewer /> : <CesiumViewer />}
         <SplitSlider />
       </main>
       {/* <RightSidebar /> */}
